db648da0e58abb756b5be44c88a56806a86f09b1
MLASS-Dilshan <sachira.sandeepa14@gmail.com>
2025-08-17 18:08:20 +0530

    Authentication completed

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Navbar from "./components/Navbar";
+import { ToastContainer, toast } from 'react-toastify';
+import PrivateRoute from "./components/PrivateRoute";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+              <Route path="/profile" element={<Profile /> } />
+          </Route>
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+        <Navbar />
+      </Router>
+
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,101 @@
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+
+const Profile = () => {
+  const auth = getAuth();
+
+  const [changedDetails, setChangedDetails] = useState(false);
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  const onSubmit = async() => {
+    try {
+      if(auth.currentUser.displayName !== name) {
+        //update display name in firebase
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        })
+
+        //update in firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name
+        })
+      }
+    } catch (err) {
+      toast.error('Could not update profile details')
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id] : e.target.value
+    }))
+  }
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changedDetails && onSubmit();
+              setChangedDetails((prevState) => !prevState);
+            }}
+          >
+            {changedDetails ? "done" : "change"}
+          </p>
+        </div>
+
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changedDetails ? "profileName" : "profileNameActive"}
+              disabled={!changedDetails}
+              value={name}
+              onChange={onChange}
+            />
+
+            <input
+              type="text"
+              id="email"
+              className={!changedDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changedDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Profile;
