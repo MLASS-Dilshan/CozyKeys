95b98b622b9f9e9286621515aff84f1b3f877f19
MLASS-Dilshan <sachira.sandeepa14@gmail.com>
2025-08-31 09:26:51 +0530

    Offers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
 
 const Offers = () => {
-  return (
-    <div>Offers</div>
-  )
-}
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default Offers
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        //Get reference
+
+        const listingsRef = collection(db, "listings");
+
+        // Create a query
+
+        const q = query(
+          listingsRef,
+          where("offer", "==", true),
+          orderBy("timestamp", "desc"),
+          limit(10)
+        );
+
+        // Execute Query
+
+        const querySnap = await getDocs(q);
+
+        const listings = [];
+
+        querySnap.forEach((doc) => {
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+
+        setListings(listings);
+        setLoading(false);
+
+        console.log(listings);
+      } catch (error) {
+        toast.error("Error fetching data!");
+      }
+    };
+
+    // console.log("Current category param:", params.categoryName);
+    fetchListings();
+  }, []);
+
+  return (
+    <div className="category">
+      <header>
+        <p className="pageHeader">
+          Offers
+        </p>
+      </header>
+
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className="categoryListings">
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>There are no current offers!</p>
+      )}
+    </div>
+  );
+};
+
+export default Offers;
