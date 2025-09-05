a9329c915894634b4b503430b6c91a16e2facf60
MLASS-Dilshan <sachira.sandeepa14@gmail.com>
2025-09-05 08:54:03 +0530

    Contact landlord page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateLisitngs from "./pages/CreateLisitngs";
 import Listings from "./pages/Listings";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
           <Route path="/forgot-password" element={<ForgotPassword />} />
            <Route path="/create-listing" element={<CreateLisitngs />} />
            <Route path="/category/:categoryName/:listingId" element={<Listings />}/>
+           <Route path="/contact/:landlordId" element = {<Contact />}/>
         </Routes>
         <Navbar />
       </Router>
--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -63,35 +63,44 @@ const Listings = () => {
                 .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </p>
 
-        <p className="listingLocation">
-            {listing.location}
-        </p>
+        <p className="listingLocation">{listing.location}</p>
 
         <p className="listingType">
-            For {listing.type === 'rent' ? 'Rent' : 'Sale'}
+          For {listing.type === "rent" ? "Rent" : "Sale"}
         </p>
 
         {listing.offer && (
-            <p className="discountPrice">
-                ${listing.regularPrice - listing.discountedPrice} discount
-            </p>
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
         )}
 
         <ul className="listingDetailsList">
-            <li>{listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}</li>
-            <li>{listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}</li>
-            <li>{listing.parking && 'Parking Spot'}</li>
-             <li>{listing.furnished && 'Furnished'}</li>
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : "1 Bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : "1 Bathroom"}
+          </li>
+          <li>{listing.parking && "Parking Spot"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
         </ul>
 
         <p className="listingLocationTitle">Location</p>
 
         {/* Map */}
 
-        {auth.currentUser?.uid !== listing.userRef && ( 
-            <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className="primaryButton">
-                Contact Landlord
-            </Link>
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className="primaryButton"
+          >
+            Contact Landlord
+          </Link>
         )}
       </div>
     </main>
